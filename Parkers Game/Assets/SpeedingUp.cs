8a9ea25c4f5e72cffbf25ae12e508c78a85df3f2
pah351 <31699511+pah351@users.noreply.github.com>
2017-09-18 01:36:48 -0400

    Created the environment for the game

---
+++ Parkers Game/Assets/SpeedingUp.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameControl : MonoBehaviour {
+	public static GameControl instance;
+	public Text distanceText;
+
+	private int distance = 0;
+	public float scrollSpeed = -1.5f;
+
+	void Awake (){
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+	}
+
+	void Update(){
+	}
+}
