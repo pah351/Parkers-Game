6adb8273c4964619778fa747047e0579265065b2
Parker Hollingsworth <pah351@nyu.edu>
2017-09-19 18:48:59 -0400

    Fixed scrolling background to scroll properly

--- Parkers Game/Assets/scrolling.cs
+++ Parkers Game/Assets/scrolling.cs
@@ -10,13 +10,13 @@ public class scrolling : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-		//startPos = transform.position;
+		startPos = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		float newPos = Mathf.Repeat(Time.time * scrollSpeed, 20);
+		float newPos = Mathf.Repeat(Time.time * scrollSpeed, 38);
 		transform.position = startPos + Vector2.right * newPos;
 	}
 }
